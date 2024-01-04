a8fae3cf5b8c8273228d8fed99103ebf148db32e
ryan910707 <ryanke91@gmail.com>
2024-01-05 01:11:58 +0800

    add emoji

--- CUDA/correctness_test.sh
+++ CUDA/correctness_test.sh
@@ -8,9 +8,10 @@ OUTPUT_CENTROID="output_centroid_$NUM"
 srun -N1 -n1 --gres=gpu:1 ./main $K "$INPUT_FILE" "$OUTPUT_DATAPOINTS" "$OUTPUT_CENTROID"
 
 if diff "../Sequential/$OUTPUT_DATAPOINTS" "$OUTPUT_DATAPOINTS" >/dev/null; then
-    echo "Files are identical"
+    echo -e "\e[32m😊 Correct\e[0m"  # Green smiley face emoji
 else
-    echo "Files are different! Something is wrong."
+    echo -e "\e[31m☹️ Wrong.\e[0m"  # Red sad face emoji
 fi
+
 
 rm "$OUTPUT_DATAPOINTS"
