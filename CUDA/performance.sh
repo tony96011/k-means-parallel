5f517873da2aa63d2832ecc41d262b5dd6d8b447
ryan910707 <ryanke91@gmail.com>
2024-01-06 00:10:55 +0800

    solve SE

--- CUDA/performance.sh
+++ CUDA/performance.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-for NUM in 1000 10000 100000 500000 1000000; do
+for NUM in 1000000; do
     srun -N1 -n1 --gres=gpu:1 ./main 5 ../input/input_$NUM.txt output_datapoints_.txt output_centroid_.txt
 done
 
 rm output_datapoints_*
 
 
+## test for different N, K 
+##  
